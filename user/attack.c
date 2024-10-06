d353e2f99ac846274af965f7ca1c38309ccc3b05
Matus Bednarik <matusbednarik197@gmail.com>
2024-10-06 13:44:14 +0200

    Finished attack.c

--- user/attack.c
+++ user/attack.c
@@ -2,12 +2,37 @@
 #include "kernel/fcntl.h"
 #include "user/user.h"
 #include "kernel/riscv.h"
+#include "kernel/param.h"
+
+
+// Define the size of the secret
+#define SECRET_SIZE 8
+
+int
+strncmp(const char *p, const char *q, uint n)
+{
+  while(n > 0 && *p && *p == *q)
+    n--, p++, q++;
+  if(n == 0)
+    return 0;
+  return (uchar)*p - (uchar)*q;
+}
+
 
 int
 main(int argc, char *argv[])
 {
-  // your code here.  you should write the secret to fd 2 using write
-  // (e.g., write(2, secret, 8)
+    char *start = sbrk(PGSIZE * 32);
+    char *end = start + PGSIZE * 32;
+    const char *target = "very very secret pw is:";
 
-  exit(1);
+    for (char *ptr = start; ptr < end - SECRET_SIZE; ptr++) {
+        if (strncmp(ptr, target, strlen(target)) == 0) {
+            char *after_colon = ptr + strlen(target) + 1;
+            if (strlen(after_colon) == 7) {
+                write(2, after_colon, 8);
+            }
+        }
+    }
+    exit(0);
 }

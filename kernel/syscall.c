c5fa6e452a6dfc2eba750050a655c7c0ac10eed2
Matus Bednarik <matusbednarik197@gmail.com>
2024-12-01 18:36:33 +0100

    First task

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -101,6 +101,7 @@ extern uint64 sys_unlink(void);
 extern uint64 sys_link(void);
 extern uint64 sys_mkdir(void);
 extern uint64 sys_close(void);
+extern uint64 sys_symlink(void);
 
 // An array mapping syscall numbers from syscall.h
 // to the function that handles the system call.
@@ -124,6 +125,7 @@ static uint64 (*syscalls[])(void) = {
 [SYS_mknod]   sys_mknod,
 [SYS_unlink]  sys_unlink,
 [SYS_link]    sys_link,
+[SYS_symlink]    sys_symlink,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 };
